3af79be40336e1f07a4b5753ce8a0b2726683dab
Boyi Ma <boyima98@gmail.com>
2020-11-29 22:41:32 -0500

    initial commit of the server

--- server/Client.cpp
+++ server/Client.cpp
@@ -1,76 +1,111 @@
-// Name: Boyi Ma
-// ID: 250892829
 #include "thread.h"
 #include "socket.h"
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <string>
+#include <thread>
 
 using namespace Sync;
+
+void threadRead(Socket& socketReference)
+{
+    ByteArray data;
+    while (true)
+    {
+        socketReference.Read(data);
+        std::cout << data.ToString() << std::endl;
+    }
+};
+
+void threadWrite(Socket& socketReference)
+{
+    ByteArray data;
+    std::string userInput;
+    while (true)
+    {
+        getline(std::cin, userInput);
+        data = ByteArray(userInput);
+        socketReference.Write(data);
+    }
+};
 
 int main(void)
 {
 	// Welcome the user 
-	std::cout << "SE3313 Lab 3 Client" << std::endl;
-    std::cout << "**Enter done to exit**" << std::endl;
+	std::cout << "I am the auction house client" << std::endl;
+//    std::cout << "**Enter done to exit**" << std::endl;
 
     std::string userInput, serverResponse;
     ByteArray data;
+    Socket socket("127.0.0.1", 3001);
 
-	try
-	{
-        // Create our socket
-        Socket socket("127.0.0.1", 3001);
+    //open the socket
+    socket.Open();
 
-        //open the socket
-        socket.Open();
+    Socket& socketReference = socket;
 
-        while (true)
-        {
-            std::cout << "Please enter a string: " << std::endl;
-            getline(std::cin, userInput);
+    std::thread th1(threadRead, socket);
+    std::thread th2(threadWrite, socket);
 
-            //if done, exit the loop to close socket
-            if (userInput == "done")
-                break;
+    th1.join();
+    th2.join();
+    return 0;
 
-            //populate ByteArray using userInput
-            data = ByteArray(userInput);
 
-            //check number of bytes successfully written
-            if (socket.Write(data) != data.v.size())
-            {
-                std::cout << "Socket has been closed at the server side." << std::endl;
-                break;
-            }
-
-            socket.Read(data);
-
-            //check the result of read
-            if (data.v.size() == 0) {
-                std::cout << "Socket was gracefully closed at the server side." << std::endl;
-                break;
-            }
-
-            else if (data.v.size() < 0) {
-                std::cout << "Read error!" <<std::endl;
-                break;
-            }
-
-            //read looks good, continue for next user input
-            else {
-                serverResponse = data.ToString();
-                std::cout << serverResponse <<std::endl;
-            }
-        }
-
-        socket.Close();
-        return 0;
-    }
-    catch (std::string e)
-    {
-        std::cout << e << std::endl;
-        return 1;
-    }
+//	try
+//	{
+//        // Create our socket
+//        Socket socket("127.0.0.1", 3001);
+//
+//        //open the socket
+//        socket.Open();
+//
+//        while (true)
+//        {
+//            std::cout << "Please enter a string: " << std::endl;
+//            getline(std::cin, userInput);
+//
+//            //if done, exit the loop to close socket
+//            if (userInput == "done")
+//                break;
+//
+//            //populate ByteArray using userInput
+//            data = ByteArray(userInput);
+//
+//            //check number of bytes successfully written
+//            if (socket.Write(data) != data.v.size())
+//            {
+//                std::cout << "Socket has been closed at the server side." << std::endl;
+//                break;
+//            }
+//
+//            socket.Read(data);
+//
+//            //check the result of read
+//            if (data.v.size() == 0) {
+//                std::cout << "Socket was gracefully closed at the server side." << std::endl;
+//                break;
+//            }
+//
+//            else if (data.v.size() < 0) {
+//                std::cout << "Read error!" <<std::endl;
+//                break;
+//            }
+//
+//            //read looks good, continue for next user input
+//            else {
+//                serverResponse = data.ToString();
+//                std::cout << serverResponse <<std::endl;
+//            }
+//        }
+//
+//        socket.Close();
+//        return 0;
+//    }
+//    catch (std::string e)
+//    {
+//        std::cout << e << std::endl;
+//        return 1;
+//    }
 }
--- server/Server.cpp
+++ server/Server.cpp
@@ -1,5 +1,3 @@
-// Name: Boyi Ma
-// ID: 250892829
 #include "thread.h"
 #include "socketserver.h"
 #include <stdlib.h>
@@ -11,6 +9,140 @@
 
 using namespace Sync;
 
+// data structure for auction items
+struct auctionItem
+{
+    std::string name;
+    double startingPrice;
+};
+
+struct player
+{
+    Socket& socketReference;
+    std::string name;
+    double money;
+    std::vector<std::string> bag;
+};
+
+// this thread handles auction process
+class RoomThread : public Thread
+{
+private:
+    int roomNo;
+    int capacity;
+    std::vector<auctionItem> roomItems;
+    // reference to players in the room
+    std::vector<player> players;
+    //if the game has started
+    bool joinable = true;
+
+public:
+    bool IsJoinable()
+    {
+        return joinable;
+    }
+
+    void AddPlayer(player p)
+    {
+        players.push_back(p);
+    }
+
+    RoomThread(int roomNo, int capacity, std::vector<auctionItem> roomItems)
+    {
+        this->capacity = capacity;
+        this->roomNo = roomNo;
+        this->roomItems = roomItems;
+    }
+
+    virtual long ThreadMain()
+    {
+        //if we don't have enough players yet, don't start the game
+        while (capacity != players.size())
+        {
+            sleep(1);
+        }
+
+        ByteArray data;
+        std::string msg, userInput;
+
+        //records which player is the last bid and the bid amount
+        int lastBid;
+        double bid;
+
+        //prevent join if has enough players
+        this->joinable = false;
+
+        // welcome players
+        for (auto player : players)
+        {
+            msg = "**Welcome " + player.name + "! The game will start now**\n";
+            data = ByteArray(msg);
+            player.socketReference.Write(data);
+        }
+
+        for (int i = 0; i<roomItems.size(); i++)
+        {
+            for (int j = 0; j<players.size(); j++)
+            {
+                msg = "For "+roomItems[i].name+", the starting price is "+std::to_string(roomItems[i].startingPrice);
+
+                //if the player doesn't have enough, pass to next player
+                if (players[j].money<roomItems[i].startingPrice)
+                {
+                    msg += "\n**You don't have enough money to bid. Pass automatically**";
+                    data = ByteArray(msg);
+                    players[j].socketReference.Write(data);
+                    continue;
+                }
+                else
+                {
+                    msg = "\nWould you like to bid or pass?";
+                    data = ByteArray(msg);
+                    players[j].socketReference.Write(data);
+                }
+
+                players[j].socketReference.Read(data);
+                userInput = data.ToString();
+                if (userInput == "pass") continue;
+                else
+                {
+                    bid = std::stod(userInput);
+                    // if the bid is valid
+                    if (bid <= players[j].money && bid>=roomItems[i].startingPrice)
+                    {
+                        //update price and record the player
+                        roomItems[i].startingPrice = bid;
+                        lastBid = j;
+                        msg = "Your bid is " + userInput + ". Waiting for other players to bid...\n";
+                        data = ByteArray(msg);
+                        players[j].socketReference.Write(data);
+                    }
+                    else
+                    {
+                        msg = "**Your bid is not valid. Pass automatically**";
+                        data = ByteArray(msg);
+                        players[j].socketReference.Write(data);
+                    }
+                }
+            }
+
+            players[lastBid].money -= roomItems[i].startingPrice;
+            players[lastBid].bag.push_back(roomItems[i].name);
+
+            //announce the result
+            for (auto player : players)
+            {
+                msg = "\nCongratulate " + players[lastBid].name +" for getting " + roomItems[i].name + "!\n";
+                player.socketReference.Write(msg);
+            }
+        }
+        return 0;
+    }
+};
+
+// a global vector storing reference to room threads
+std::vector<RoomThread*> roomThreads;
+
 // This thread handles each socket connection
 class SocketThread : public Thread
 {
@@ -18,15 +150,9 @@ private:
     // Reference to the connected socket
     Socket& socketReference;
 
-    // The data we are receiving
-    ByteArray data;
-
-    // Are we terminating?
-    bool& terminate;
-    
 public:
-    SocketThread(Socket& socketReference, bool& terminate)
-    : socketReference(socketReference), terminate(terminate)
+    SocketThread(Socket& socketReference)
+            : socketReference(socketReference)
     {}
 
     ~SocketThread()
@@ -39,53 +165,135 @@ public:
 
     virtual long ThreadMain()
     {
-    	std::string userInput;
-        // If terminate is ever flagged, we need to gracefully exit
-        while(!terminate)
+        std::string name;
+        double money;
+        int roomNo;
+        ByteArray data;
+
+        try
         {
-            try
+            data = ByteArray("What is your name?");
+
+            // Send msg to player and check number of bytes successfully written
+            if (socketReference.Write(data) != data.v.size())
             {
+                std::cout << "Socket has been closed at the client side before joining a room." << std::endl;
+                return 0;
+            }
+
+            // Wait for data
+            socketReference.Read(data);
+
+            //check the result of read
+            if (data.v.size() == 0)
+            {
+                std::cout << "Socket has been closed at the client side before joining a room." << std::endl;
+                return 0;
+            }
+
+            else if (data.v.size() < 0)
+            {
+                std::cout << "Read error!" <<std::endl;
+                return 0;
+            }
+
+            //read looks good
+            else
+            {
+                name = data.ToString();
+            }
+
+            data = ByteArray("How much do you have?");
+
+            // ask for money
+            if (socketReference.Write(data) != data.v.size())
+            {
+                std::cout << "Socket has been closed at the client side before joining a room." << std::endl;
+                return 0;
+            }
+
+            // Wait for data
+            socketReference.Read(data);
+
+            //check the result of read
+            if (data.v.size() == 0)
+            {
+                std::cout << "Socket has been closed at the client side before joining a room." << std::endl;
+                return 0;
+            }
+
+            else if (data.v.size() < 0)
+            {
+                std::cout << "Read error!" <<std::endl;
+                return 0;
+            }
+
+            //read looks good, convert to money
+            else
+            {
+                money = std::stod(data.ToString());
+            }
+
+            // try to join the player into a room
+            while (true)
+            {
+                data = ByteArray("What is the room number you would like to join?");
+
+                // Send msg to player and check number of bytes successfully written
+                if (socketReference.Write(data) != data.v.size())
+                {
+                    std::cout << "Socket has been closed at the client side before joining a room." << std::endl;
+                    return 0;
+                }
+
                 // Wait for data
                 socketReference.Read(data);
-                
+
                 //check the result of read
                 if (data.v.size() == 0)
                 {
-                    std::cout << "Socket has been closed at the client side." << std::endl;
-                    break;
+                    std::cout << "Socket has been closed at the client side before joining a room." << std::endl;
+                    return 0;
                 }
 
                 else if (data.v.size() < 0)
                 {
                     std::cout << "Read error!" <<std::endl;
-                    break;
+                    return 0;
                 }
 
-                //read looks good
+                //read looks good, convert to integer
                 else
                 {
-                    userInput = data.ToString();
-                }
-                
-                // convert every character of input to uppercase
-                for (int i=0; i < userInput.length(); i++)
-                {
-                    userInput[i] = toupper(userInput[i]);
-                }
+                    roomNo = std::stoi(data.ToString());
+                    //check if we have a room the player wants to join
+                    if (roomNo > roomThreads.size())
+                    {
+                        data = ByteArray("Room does not exist. Try again.");
+                        socketReference.Write(data);
+                        continue;
+                    }
 
-                data = ByteArray(userInput);
+                    //check if room is still joinable
+                    if (!roomThreads[roomNo-1]->IsJoinable())
+                    {
+                        data = ByteArray("Game has started. Try another room.");
+                        socketReference.Write(data);
+                        continue;
+                    }
 
-                // Send it back and check number of bytes successfully written
-                if (socketReference.Write(data) != data.v.size())
-                {
-                    std::cout << "Socket has been closed at the client side." << std::endl;
+                    //put player into the room
+                    player p = {socketReference, name, money};
+                    roomThreads[roomNo-1]->AddPlayer(p);
+                    data = ByteArray("You have been added to the room. Waiting for the game to start...");
+                    socketReference.Write(data);
                     break;
                 }
             }
-            catch (...)
-            {
-                // We catch the exception, but there is nothing for us to do with it here. Close the thread.
-            }
+        }
+        catch (...)
+        {
+            // We catch the exception, but there is nothing for us to do with it here. Close the thread.
         }
         return 0;
     }
@@ -139,7 +347,7 @@ public:
 
                 // Pass a reference to this socket to create a new socket thread
                 Socket& socketReference = *newConnection;
-                socketThreads.push_back(new SocketThread(socketReference, terminate));
+                socketThreads.push_back(new SocketThread(socketReference));
             }
             catch (TerminationException termE)
             { //if the server was shutdown
@@ -161,11 +369,14 @@ public:
 
 int main(void)
 {
-    std::cout << "I am a server." << std::endl;
-    std::cout << "**Enter ""done"" to terminate the server**" << std::endl;
+    std::cout << "I am the auction house server." << std::endl;
+    std::cout << "******Enter ""exit"" to terminate the server******" << std::endl;
     
     std::string userInput;
-    
+    double userPrice;
+    int capacity, roomNo = 0;
+    std::vector<auctionItem> roomItems{};
+
     try
     {
         // try to create our server
@@ -174,18 +385,84 @@ int main(void)
         // Need a thread to perform server operations
         ServerThread serverThread(server);
 
+        //ask whether to create new room
         while (true)
         {
-            // This will wait for input to shutdown the server
-//            FlexWait cinWaiter(1, stdin);
-//            cinWaiter.Wait();
-            
+            std::cout << "**Type y to create a new room**" << std::endl;
             getline(std::cin, userInput);
 
-            if (userInput == "done")
+            // This will wait for input to shutdown the server
+            if (userInput == "exit")
             {
                 break;
             }
+
+            if (userInput != "y" && userInput != "yes")
+            {
+                std::cout << "Invalid input! Try again." << std::endl;
+                continue;
+            }
+
+            while (true)
+            {
+                std::cout << "How many players should this room take?" << std::endl;
+                if (std::cin >> capacity) {// valid number
+                    //ignore new line char for getline
+                    std::cin.ignore(1000, '\n');
+                    break;
+                } else {// not a valid number
+                    std::cout << "Invalid Input! Please input an integer." << std::endl;
+                    //clear error on cin
+                    std::cin.clear();
+                    while (std::cin.get() != '\n') ; // empty loop
+                }
+            }
+
+            //ask item names and prices
+            while (true)
+            {
+                std::cout << "What item would you like to add? (Enter done to finish adding)" << std::endl;
+                getline(std::cin, userInput);
+
+                // if done
+                if (userInput == "done")
+                {
+                    //check if we have items to add
+                    if (roomItems.empty())
+                    {
+                        std::cout << "Add at least 1 item to continue..." <<std::endl;
+                        continue;
+                    }
+                    //if we do, break out
+                    break;
+                }
+
+                // ask price and check input
+                while (true)
+                {
+                    std::cout << "How much should the starting price of " << userInput << " be?" << std::endl;
+                    if (std::cin >> userPrice) {// valid number
+                        //ignore new line char for getline
+                        std::cin.ignore(1000, '\n');
+                        break;
+                    } else {// not a valid number
+                        std::cout << "Invalid Input! Please input a numerical value." << std::endl;
+                        //clear error on cin
+                        std::cin.clear();
+                        while (std::cin.get() != '\n') ; // empty loop
+                    }
+                }
+
+                //if all checks passed, add this item
+                auctionItem userItem = {userInput, userPrice};
+                roomItems.push_back(userItem);
+            }
+            //create a new room thread and pass items
+            ++roomNo;
+            roomThreads.push_back(new RoomThread(roomNo,capacity,roomItems));
+
+            // clear items to start again
+            roomItems.clear();
         }
 
         // Shut down and clean up the server
