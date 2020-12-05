0f2c64bf2980474118d738f7bfdcaeb3a22b5fbd
Boyi Ma <boyima98@gmail.com>
2020-12-05 14:44:58 -0500

    add exit functionality to recv_msg

--- client/client.py
+++ client/client.py
@@ -1,30 +1,38 @@
 # import necessary packages
-import errno
 import socket
 import threading
 import sys
+
 
 # --- functions ---
 def recv_msg():
     # wait to receive message from the server and print it out
     while True:
-        recv_msg = s.recv(1024)
-        # if length of recv is 0, the other side has closed the socket
-        if len(recv_msg) == 0:
-            print("RECV THREAD: Connection has been gracefully terminated at the server side.")
-            s.close()  # close the socket on client side as well
+        try:
+            recv_msg = s.recv(1024)
+            # if length of recv is 0, the other side has closed the socket
+            if len(recv_msg) == 0:
+                print("RECV THREAD: Connection has been gracefully terminated at the server side.")
+                s.close()  # close the socket on client side as well
+                sys.exit(0) # exit thread
+            if not recv_msg:
+                sys.exit(0)
+            # if message is successfully received, decode and print it out
+            recv_msg = recv_msg.decode()
+            print(recv_msg)
+        except Exception:
+            # socket.recv will throw an exception if the socket is closed in the send_msg function,
+            # but that's ok, just exit
             sys.exit(0)
-        if not recv_msg:
-            sys.exit(0)
-        recv_msg = recv_msg.decode()
-        print(recv_msg)
+
 
 def send_msg():
     # collect any user input and send to server
     while True:
         send_msg = input()
         # the user may shutdown the client if they enter 'quit'
-        if send_msg == 'QUIT':
+        if send_msg == 'exit':
+            s.close()
             sys.exit(0)
         send_msg = send_msg.encode()
         # try to send thru socket
@@ -35,13 +43,14 @@ def send_msg():
             print("SEND THREAD: Connection has been gracefully terminated at the server side.")
             break
 
+
 # --- main ---
 host = '192.168.2.113'  # IP of the server
 port = 3001  # which port the server is operating on
 
 # welcome message and instructions
 print('Welcome to The SE3313 Auction House Client Application\n')
-print('You May Exit This Application at Any Time By Typing "QUIT" ')
+print('******Enter exit to terminate the client******')
 
 # create a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
